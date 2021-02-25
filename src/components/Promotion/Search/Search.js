1fddfd10c7c2293c9d9edc9f77d0fe42177de1ad
hidylima <hidy63@gmail.com>
2021-02-25 08:48:02 -0300

    add loading
    finalizado

--- src/components/Promotion/Search/Search.js
+++ src/components/Promotion/Search/Search.js
@@ -1,22 +1,40 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import PromotionCard from "../Card/Card";
+import PromotionList from "../List/List";
+import "./Search.css";
 
 const PromotionSearch = () => {
   const [promotions, setPromotions] = useState([]);
+  const [search, setSearch] = useState("");
+  const params = {};
 
+  if (search) {
+    params.title_like = search;
+  }
   useEffect(() => {
     axios
-      .get("http://localhost:3004/promotions?_embed=comments")
+      .get("http://localhost:3004/promotions?_embed=comments", { params })
       .then((response) => {
         setPromotions(response.data);
       });
-  }, []);
+  }, [search]);
+
   return (
-    <div>
-      {promotions.map((promotion) => (
-        <PromotionCard promotion={promotion} />
-      ))}
+    <div className="promotion-search">
+      <header className="promotion-search__header">
+        <h1>Promo show</h1>
+        <Link to="/create">Novo Produto</Link>
+      </header>
+      <input
+        className="promotion-search__input"
+        placeholder="Buscar"
+        type="search"
+        name="seach"
+        id="search"
+        onChange={(ev) => setSearch(ev.target.value)}
+      />
+      <PromotionList promotions={promotions} loading={!promotions.length} />
     </div>
   );
 };
--- src/pages/Promotion/Search/Search.js
+++ src/pages/Promotion/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PromotionSearch from "components/Promotion/Search/Search";
 import UIContainer from "components/UI/Container/Container";
 
